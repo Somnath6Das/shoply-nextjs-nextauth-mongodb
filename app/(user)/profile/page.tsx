b9c50725ac3e3a4e2ddbbadaebb4d6e34e9ebaff
Somnath Das <somnath8das@gmail.com>
2025-11-28 20:25:40 +0530

    dynamic-product-page

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -4,11 +4,13 @@ import { signOut } from "next-auth/react";
 
 export default function Profile() {
   return (
-    <button
-      onClick={() => signOut({ callbackUrl: "/" })}
-      className="px-4 py-2 bg-red-600 text-white rounded-lg"
-    >
-      Logout
-    </button>
+    <div className="bg-gray-50 h-screen">
+      <button
+        onClick={() => signOut({ callbackUrl: "/" })}
+        className="px-4 py-2 bg-red-600 text-white rounded-lg"
+      >
+        Logout
+      </button>
+    </div>
   );
 }
